eb5ae368f4a1f1cd60e631d3468d4cbcc00df642
Alejandro <acuellarh@gmail.com>
2019-10-16 06:32:23 -0500

    status: :created, configured in products_controller and routes.rb updated

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,22 +1,35 @@
 class Api::V1::ProductsController < ApplicationController
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   
   def index
-    render json: Product.all
+    @products = Product.all
+    render json: @products
   end
 
+  def new
+    @product = Product.new
+  end  
+
   def create
-    product = Product.new(product_params)
-    if product.save
-      render json: product
+    @product = Product.new(product_params)
+    if @product.save
+      render json: product, status: :created
     else
-      render json: { errors: product.errors }, status: 422
+      render json: { errors: @product.errors }, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])    
+  end
+
+  def update
+    @product = Product.update(params[:id], product_params)    
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :price, :created_at)
+    params.require(:product).permit(:name, :price)
   end
 
 end
